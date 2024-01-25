73602613df1a34607298cd4bdc800c1e980b1041
Picafres4 <al02921139@tecmilenio.mx>
2024-01-25 14:39:09 -0600

    Initial Commit

---
+++ actividad2_iOS_Rada/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  actividad2_iOS_Rada
+//
+//  Created by athaína rada on 25/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var labelNumer: UILabel!
+    
+    @IBOutlet weak var inputNumber: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func guessButton(_ sender: UIButton) {
+        let randomNumber = Int(arc4random_uniform(6)+1)
+        labelNumer.text = String(randomNumber)
+        
+        if inputNumber.text == labelNumer.text{
+            view.backgroundColor = UIColor.green
+        } else{
+            view.backgroundColor = UIColor.red
+        }
+    }
+    
+}
+
